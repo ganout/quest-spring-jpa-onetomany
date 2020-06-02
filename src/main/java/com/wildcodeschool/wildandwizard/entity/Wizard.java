a1a173c2c6f6c2d4a8e2b79853fd50eb2dd5948c
ganout <morgan.bourdillel@gmail.com>
2020-06-02 10:21:32 +0200

    added possibility registration student in a school

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,9 +1,6 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
@@ -19,6 +16,10 @@ public class Wizard {
     private String biography;
     private boolean muggle;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH, optional = false)
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
+
     public Wizard() {
     }
 
@@ -29,6 +30,7 @@ public class Wizard {
     public void setId(Long id) {
         this.id = id;
     }
+
 
     public String getFirstName() {
         return firstName;
@@ -76,5 +78,14 @@ public class Wizard {
 
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
+    }
+
+    public School getSchool(){
+
+        return this.school;
+    }
+
+    public void setSchool(School newSchool){
+        this.school = newSchool;
     }
 }
